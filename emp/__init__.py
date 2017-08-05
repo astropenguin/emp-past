81a41290b09bd8580a5b5e9993e5f40bfbd15616
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-06 01:23:09 +0900

    Update __init__.py

--- emp/__init__.py
+++ emp/__init__.py
@@ -1,1 +1,19 @@
 # coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .cmds import *
+from .logging import *
+from .utils import *
+del cmds
+del logging
+del utils
+
+# default logger
+import logging
+logger = logging.getLogger('emp')
+setlogfile(logger=logger)
+setloglevel(logger=logger)
+del logging
