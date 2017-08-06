297ea2df1753ab7302ba5965033551ea587f79ce
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-06 23:37:11 +0900

    Update __init__.py

--- emp/__init__.py
+++ emp/__init__.py
@@ -5,7 +5,7 @@ from __future__ import print_function as _print_function
 from __future__ import unicode_literals as _unicode_literals
 
 # information
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__  = 'snoopython'
 
 # submodules
